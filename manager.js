da0ab3d4997a9cf39bc81826ff34239fd924bf37
DanteSM <88889073+DanteSM@users.noreply.github.com>
2023-04-27 23:34:48 +0530

    Update manager.js

--- manager.js
+++ manager.js
@@ -1,18 +1,17 @@
 
 
 
-  var passmap2 = new Map();
-
+  var passmap4 = new Map();
   document.getElementById('new-todo-form').addEventListener('submit', (event) => {
     event.preventDefault();
     const user = document.getElementById('content').value;
     const pass = document.getElementById('content1').value;
-    passmap2.set(user,pass)
-    let listpass = "";
-    for (const iter of passmap2.entries()) {
-      listpass += iter + "\n";
+    passmap5.set(user,pass)
+    
+    for (var iterA of passmap5.entries()) {
+      listpass2 += iterA + "\n";
      }
-    alert(listpass);
+    alert(listpass2);
 
   
 
